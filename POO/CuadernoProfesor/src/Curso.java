8611dc8383e80be1a3e48edf9181db011162d083
Davidav0 <55433713+Davidav0@users.noreply.github.com>
2019-12-03 18:14:11 +0100

    CuadernoProfesor (Aún sin implementar XML)

---
+++ POO/CuadernoProfesor/src/Curso.java
@@ -0,0 +1,61 @@
+import java.util.LinkedList;
+
+public class Curso {
+
+    String id;
+    String nombre;
+    LinkedList<Asignatura> asignaturas;
+    //LinkedList<Alumno> alumnos;
+
+    public Curso() {}
+
+    public Curso(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        asignaturas = new LinkedList<>();
+    }
+
+    public void addAsignaturaAlCurso(Asignatura asignatura) {
+        asignaturas.add(asignatura);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LinkedList<Asignatura> getAsignaturas() {
+        return asignaturas;
+    }
+
+    public void setAsignaturas(LinkedList<Asignatura> asignaturas) {
+        this.asignaturas = asignaturas;
+    }
+/*
+    public LinkedList<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(LinkedList<Alumno> alumnos) {
+        this.alumnos = alumnos;
+    }
+*/
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
---
+++ POO/CuadernoProfesor/src/MainCuaderno.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+
+public class MainCuaderno {
+
+    public static void main(String[] args) {
+        Asignatura Programacion = new Asignatura("01", "Programación");
+        Asignatura EntornosDeDesarrollo = new Asignatura("02", "Entornos de Desarrollo");
+        Asignatura LenguajeDeMarcas = new Asignatura("03", "Lenguaje de Marcas");
+
+        Curso S11AW = new Curso("S11AW", "Desarrollo de Aplicaciones Web");
+
+        S11AW.addAsignaturaAlCurso(Programacion);
+        S11AW.addAsignaturaAlCurso(EntornosDeDesarrollo);
+        S11AW.addAsignaturaAlCurso(LenguajeDeMarcas);
+
+        Alumno David = new Alumno("David Fdez", "0001");
+        Alumno Roberto = new Alumno("Roberto Miguel", "0002");
+
+        Programacion.matricularAlumno(David);
+        Programacion.matricularAlumno(Roberto);
+
+        Programacion.setIndicadorAlumno(David, new Indicador(David.getNombreCompleto(), Programacion, "Examen trimestral", 7.5));
+        Programacion.setIndicadorAlumno(David, new Indicador(David.getNombreCompleto(), Programacion, "Examen trimestral", 6.5));
+        Programacion.setIndicadorAlumno(Roberto, new Indicador(Roberto.getNombreCompleto(), Programacion, "Examen trimestral", 7.5));
+        Programacion.setIndicadorAlumno(Roberto, new Indicador(Roberto.getNombreCompleto(), Programacion, "Examen trimestral", 6.5));
+
+        System.out.println(Programacion.getNotas());
+
+    }
+}
